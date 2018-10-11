d9b15a86d8226cb177282fdfb37dd80738ba13fd
Austin Green <austin.o.green-1@ou.edu>
2018-10-11 08:48:40 -0500

    Queue and Stack Example

---
+++ Driver.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Driver
+{
+	public static void main(String[] args)
+	{
+		Stack<String> meStackOVillians = new Stack<String>();
+		Queue<String> meQueueOVillians = new LinkedList<String>();
+		
+	}
+}
